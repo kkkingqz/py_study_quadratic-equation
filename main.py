4763dc438ae9cc36ba56974c505ea48f5426a370
Bodya <kkingqz@gmail.com>
2022-08-24 20:15:23 +0300

    add d == 0

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ c = int(input("Enter coefficient c: "))
 
 D = b ** 2 - 4 * a * c
 print(D)
-if D >= 0:
+if D > 0:
     x1 = (-b + math.sqrt(D)) / (2 * a)
     x2 = (-b - math.sqrt(D)) / (2 * a)
+elif D == 0:
+    x1 = (-b) / (2 * a)
+    x2 = None
 else:
     y1 = complex(-b, math.sqrt(-D))
     y2 = complex(-b, -math.sqrt(-D))
-    #x1 = y1 / (2 * a)
-    #x2 = y2 / (2 * a)
+    x1 = y1 / (2 * a)
+    x2 = y2 / (2 * a)
 
 print(x1, '   ', x2)
